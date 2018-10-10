434383d6f89f0dbb179581f8ba5bc9e4e6a50ecb
Student <student@iMac-B202-05.local>
2018-10-10 12:45:35 +0200

    bam

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslationProvider} from '../../providers/translation/translation'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private userInput:string;
+  private result:string;
+
+  constructor(public navCtrl: NavController,private translationProvider: TranslationProvider) {
 
   }
 
+  public BtnTranslateClicked(userinput:string):void{
+    console.log(userinput);
+    this.userInput = userinput;
+    this.translationProvider.getTranslationResult(userinput).subscribe((response) => {
+      this.result = response.responseData.translatedText;
+
+    });
+  }
 }
